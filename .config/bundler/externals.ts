cc9e55fb28723094aa43d0e776643e8e1cdb3c56
Albert <albert@opencode.local>
2026-06-27 07:00:30 +0000

    Fix: Remove jsx-runtime from externals (proper fix)

--- .config/bundler/externals.ts
+++ .config/bundler/externals.ts
@@ -21,8 +21,6 @@ export const externals: ExternalsType = [
   'slate-plain-serializer',
   '@grafana/slate-react',
   'react',
-  // 'react/jsx-runtime',
-  // 'react/jsx-dev-runtime',
   'react-dom',
   'react-redux',
   'redux',
